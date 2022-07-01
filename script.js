6e7f6d38a065407c9d9a31b233a5a82eea77cab4
harihaysrun <nursyahirah98@outlook.com>
2022-07-02 01:09:20 +0800

    use swipe left right for desktop

--- script.js
+++ script.js
@@ -1,14 +1,56 @@
-const container = document.getElementById("container")
+// const container = document.getElementById("container")
+const outerContainer = document.getElementById("outer-container")
+const container = document.getElementsByClassName("container")
 const bg = document.getElementsByClassName("bg")
 const workMenu = document.getElementById("work-menu")
 const popupContainer = document.getElementById("popup-container")
 const closeBtn = document.getElementById("close-popup")
 
-// container.addEventListener("wheel", function(event){
+const bgContainer = document.getElementById("background-container")
+const fgContainer = document.getElementById("foreground-container")
+
+let bgOneWidth;
+let bgTwoWidth;
+let fullWidth = 0;
+
+
+// container[0].addEventListener("wheel", function(event){
 //     event.preventDefault()
-//     container.scrollLeft += event.deltaY
+//     container[0].scrollLeft += event.deltaY
 // })
 
+// // console.log(container.length)
+// for(i=0;i<container.length;i++){
+//     console.log(container[i])
+//     container[i].addEventListener("wheel", function(event){
+//         event.preventDefault()
+//         container[i].scrollLeft += event.deltaY
+//     })
+// }
+
+console.log(
+    /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent))
+
+if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    
+    console.log("mobile")
+    container[0].style.overflow = "auto"
+
+   } else{
+    console.log("not mobile")
+    window.addEventListener("wheel", function(event){
+        // event.preventDefault()
+        // event.stopImmediatePropagation()
+        bgContainer.scrollLeft += event.deltaY
+        // fgContainer.scrollLeft += event.deltaY
+    })
+        
+}
+
+// fgContainer.addEventListener("wheel", function(event){
+//     event.preventDefault()
+//     fgContainer.scrollLeft += event.deltaY
+// })
 
 // window.addEventListener("scroll", function(event){
 //         event.preventDefault()
@@ -17,15 +59,12 @@ const closeBtn = document.getElementById("close-popup")
 //     container.scrollLeft = window.scrollX
 // })
 
-let bgOneWidth;
-let bgTwoWidth;
-
 window.addEventListener("load", getBgWidth);
-window.addEventListener("resize", getBgWidth);
+// window.addEventListener("resize", getBgWidth);
 function scrollPosition(){
-    container.addEventListener("scroll", function(){
+    container[0].addEventListener("scroll", function(){
         // else {
-            if (container.scrollLeft > (bgOneWidth - (window.innerWidth / 2))){
+            if (container[0].scrollLeft > (bgOneWidth - (window.innerWidth / 2))){
                 // workMenu.style.display = "block"
                 workMenu.classList.add("work-menu-appear")
             }
@@ -47,16 +86,16 @@ function getBgWidth(){
     }
 
     // setTimeout(function(){
-        let fullWidth = 0;
         let boxes = document.getElementsByClassName("box");
-        for(let i=0; i<boxes.length;i++){
+        for(let i=0; i<(boxes.length / 2);i++){
             fullWidth += boxes[i].getBoundingClientRect().width;
             // console.log(boxes[i].getBoundingClientRect().width)
         }
         // console.log(container.getBoundingClientRect().width)
         console.log(fullWidth)
-        container.style.height = `${fullWidth}px`;
-        container.style.width = `${fullWidth}px`;
+        container[0].style.height = `${fullWidth}px`;
+        // container[0].style.height = `${fullWidth}px`;
+
     // },2000)
     window.addEventListener("scroll", function(event){
         event.preventDefault()
@@ -65,7 +104,10 @@ function getBgWidth(){
         // window.scrollTop(0)
         // container.scrollLeft = `${window.scrollY}px`
         if(window.scrollY <= fullWidth){
-            container.style.transform = `translateX(-${window.scrollY}px)`;
+            // bgContainer
+// fgContainer
+            container[0].style.transform = `translateX(-${window.scrollY}px)`;
+            // fgContainer.style.transform = `translateX(-${window.scrollY}px)`;
         }
     })
 
@@ -121,3 +163,23 @@ prevBtn.addEventListener("click", function(){
         clientLogosContainer.scrollLeft = -clientLogosContainer.getBoundingClientRect().width;
     }
    })
+
+// window.addEventListener("touchstart", touchStart, false);
+// window.addEventListener("touchmove", touchMove, false);
+
+// var start = {x:0, y:0};
+
+// function touchStart(event) {
+//   start.x = event.touches[0].pageX;
+//   start.y = event.touches[0].pageY;
+// }
+
+// function touchMove(event) {
+//   offset = {};
+
+//   offset.x = start.x - event.touches[0].pageX;
+//   offset.y = start.y - event.touches[0].pageY;
+
+//   return offset;  
+// }
+
